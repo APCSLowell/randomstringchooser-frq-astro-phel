a80516bf41132d25d498cd097a2c373ee630371d
astro-phel <katgranads@gmail.com>
2025-06-03 02:35:30 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -7,15 +7,3 @@ public class RandomLetterChooser extends RandomStringChooser
 	}
 		
 }
-	
-	
-	public static String[] getSingleLetters(String str)
-	{ 
-		String[] letterArray = new String[str.length()];
-		for(int i=0; i<str.length(); i++)
-		{
-			letterArray[i] = ""+str.charAt(i);
-		}
-		return letterArray;
-	}
-}
