5902e168539a3dce71d2b2af25776dd400b935ce
astro-phel <katgranads@gmail.com>
2025-06-03 02:24:16 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -3,13 +3,13 @@ public class RandomLetterChooser extends RandomStringChooser
 	
 	public RandomLetterChooser(String str){ 
 		/* to be implemented in part (b) */
-		public RandomLetterChooser(String str){
-			super(smth(str));
+		super(getSingleLetters(str));
 	}
 	public static String[] getSingleLetters(String str){
-		for(int i = 0; i < str.length();i++)
-			return;
-	}
+		String[] letterArray = new String[str.length()];
+		for(int i = 0; i < str.length(); i++)
+			letterArray[i] = "" +str.charAt(i);
+		return letterArray;
 		
 }
 	
