e3aec5dbb2964a8db46afb2ff13e8c1514d57cb6
astro-phel <katgranads@gmail.com>
2025-06-03 02:38:41 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,9 +1,16 @@
-public class RandomLetterChooser extends RandomStringChooser
-{
+public class RandomLetterChooser extends RandomStringChooser{
 	
 	public RandomLetterChooser(String str){ 
 		/* to be implemented in part (b) */
 		super(getSingleLetters(str));
 	}
-		
+	
+	
+	public static String[] getSingleLetters(String str){
+		String[] letterArray = new String[str.length()];
+		for(int i=0; i<str.length(); i++){
+			letterArray[i] = ""+str.charAt(i);
+		}
+		return letterArray;
+	}
 }
